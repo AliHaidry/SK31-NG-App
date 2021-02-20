73335143fd97234c940e8bd84568dacd1f703f63
AliHaidry <alihaidry11@gmail.com>
2021-02-19 21:35:27 -0500

    Code upate

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let min = 1,
     guessesLeft = 3;
 
 // UI Elements
-const game = document.querySelector('.game'),
+const game = document.querySelector('#game'),
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
       guessBtn = document.querySelector('#guess-btn'),
@@ -36,18 +36,53 @@ guessBtn.addEventListener('click',function(){
     // Check if won
     if(guess === winningNum)
     {
-        // Disable input
-        guessInput.disabled = true;
-        // Change border color 
-        guessInput.style.borderColor = 'green';
-        // Set message
-        setMessage(`${winningNum} is correct! you win!`, 'green');
+        // Game over - won 
+        gameOver(true, `${winningNum} is correct, You Win!`);
+       
     } 
     else
     {
+        // Wrong number
+        guessesLeft -= 1;
+        
+        if(guessesLeft === 0)
+        {
+            // Game over - lost
+            gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
 
+        }
+        else
+        {
+            // Game continues - answer wrong
+
+            // Change border color
+            guessInput.style.borderColor = 'red';
+
+            // Clear Input
+            guessInput.value = '';
+
+            // Tell user its the wrong numberGuesser
+            setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+        }
     }
 });
+
+// Game Over
+function gameOver(won, msg)
+{
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = color;
+    // Set text color
+    message.style.color = color;
+    // set Message
+    setMessage(msg);
+}
+
 
 // Set message
 function setMessage(msg, color) 
